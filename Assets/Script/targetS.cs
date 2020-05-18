41d1f07cdee0ca46bed303630ca2144e836a698b
l3ad <46997806+l3ad@users.noreply.github.com>
2020-05-18 03:57:40 -0700

    Ver0
    First Time

---
+++ Assets/Script/targetS.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetS : MonoBehaviour
+{
+    float sin;
+    float timeElaps;
+    public bool bas;
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (!bas)
+        {
+            timeElaps += Time.deltaTime * 0.08f;
+            sin = Mathf.Abs(Mathf.Sin(timeElaps));
+            transform.position = new Vector3(Mathf.Lerp(7, -7, sin), 3.24f, 0);
+        }
+
+    }
+
+}
